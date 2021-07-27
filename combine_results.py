8f5a3211178f39fc4ba69b35c3015efca5e576b6
Daniel Mohr <daniel.mohr@dlr.de>
2021-07-27 21:37:41 +0200

    go on with combining artifacts

--- combine_results.py
+++ combine_results.py
@@ -8,10 +8,12 @@
 
 import json
 
+
 def read_json(filename):
     with open(filename, 'r') as fd:
         json_instance = json.load(fd)
     return json_instance
+
 
 ubuntu_1804_listing = read_json('ubuntu-1804_listing/data.json')
 ubuntu_2004_listing = read_json('ubuntu-2004_listing/data.json')
@@ -23,14 +25,16 @@ print(type(macos_1015_listing))
 
 table = []
 for i in range(24):
-    line = [i] # dtype.num
+    line = [i]  # dtype.num
     for attr in ['dtype.char', 'dtype.kind',
                  'dtype.str', 'dtype.num', 'dtype.itemsize']:
-        line.append(ubuntu_1804_listing[i][attr]) # dtype.kind
+        line.append(ubuntu_1804_listing['dtypes'][str(i)][attr])  # dtype.kind
         for data in [ubuntu_2004_listing, macos_1015_listing,
-                    windows_2016_listing, windows_2019_listing]:
-            assert(ubuntu_1804_listing[i][attr] == data[i][attr])
+                     windows_2016_listing, windows_2019_listing]:
+            assert(ubuntu_1804_listing['dtypes'][str(i)][attr] ==
+                   data['dtypes'][str(i)][attr])
     for data in [ubuntu_1804_listing, ubuntu_2004_listing, macos_1015_listing,
                  windows_2016_listing, windows_2019_listing]:
-        line.append(data[i]['dtype']) # dtype
+        line.append(data['dtypes'][str(i)]['dtype'])  # dtype
+    table.append(line)
 print(table)
