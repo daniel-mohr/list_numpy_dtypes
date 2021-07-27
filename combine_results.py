a2edc33af6ab5cfa800c2d8938957345b1fd7ed0
Daniel Mohr <daniel.mohr@dlr.de>
2021-07-27 21:28:33 +0200

    go on with combining artifacts

---
+++ combine_results.py
@@ -0,0 +1,36 @@
+#!/usr/bin/python3
+"""
+:Author: Daniel Mohr
+:Email: daniel.mohr@dlr.de
+:Date: 2021-07-27
+:License: BSD 3-Clause License
+"""
+
+import json
+
+def read_json(filename):
+    with open(filename, 'r') as fd:
+        json_instance = json.load(fd)
+    return json_instance
+
+ubuntu_1804_listing = read_json('ubuntu-1804_listing/data.json')
+ubuntu_2004_listing = read_json('ubuntu-2004_listing/data.json')
+macos_1015_listing = read_json('macos-1015_listing/data.json')
+windows_2016_listing = read_json('windows-2016_listing/data.json')
+windows_2019_listing = read_json('windows-2019_listing/data.json')
+print(type(macos_1015_listing))
+
+
+table = []
+for i in range(24):
+    line = [i] # dtype.num
+    for attr in ['dtype.char', 'dtype.kind',
+                 'dtype.str', 'dtype.num', 'dtype.itemsize']:
+        line.append(ubuntu_1804_listing[i][attr]) # dtype.kind
+        for data in [ubuntu_2004_listing, macos_1015_listing,
+                    windows_2016_listing, windows_2019_listing]:
+            assert(ubuntu_1804_listing[i][attr] == data[i][attr])
+    for data in [ubuntu_1804_listing, ubuntu_2004_listing, macos_1015_listing,
+                 windows_2016_listing, windows_2019_listing]:
+        line.append(data[i]['dtype']) # dtype
+print(table)
--- list_numpy_dtypes_json.py
+++ list_numpy_dtypes_json.py
@@ -2,7 +2,7 @@
 """
 :Author: Daniel Mohr
 :Email: daniel.mohr@dlr.de
-:Date: 2021-07-22
+:Date: 2021-07-27
 :License: BSD 3-Clause License
 """
 
@@ -21,10 +21,13 @@ dtypes_data = data['dtypes']
 for i in range(24):
     nat = numpy.array([], dtype=numpy.sctypeDict[i])
     dtypes_data[i] = dict()
-    dtypes_data[i]['dtype'] = str(numpy.sctypeDict[i])
+    # dtypes_data[i]['dtype'] = str(numpy.sctypeDict[i])
+    dtypes_data[i]['dtype'] = str(nat.dtype)
     dtypes_data[i]['dtype.char'] = nat.dtype.char
     dtypes_data[i]['dtype.kind'] = nat.dtype.kind
     dtypes_data[i]['dtype.num'] = nat.dtype.num
+    dtypes_data[i]['dtype.itemsize'] = nat.dtype.itemsize
+    dtypes_data[i]['dtype.str'] = nat.dtype.str
 
 with open('data.json', 'w') as fd:
     json.dump(data, fd)
